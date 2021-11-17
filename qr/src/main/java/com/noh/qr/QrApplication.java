31cf0434c9dc9c2ce6dbc10115451ad774991fd1
anoudeth <pisces2531@gmail.com>
2021-11-17 08:40:49 +0700

    qr

---
+++ qr/src/main/java/com/noh/qr/QrApplication.java
@@ -0,0 +1,15 @@
+package com.noh.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(QrApplication.class, args);
+
+	}
+
+}
