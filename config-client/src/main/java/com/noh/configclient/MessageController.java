c7ecf1d0666e72f457ce10cd7a2796c390c57f62
nohder <pisces2531@gmail.com>
2022-06-07 06:19:33 +0700

    add cloud config-server

---
+++ config-client/src/main/java/com/noh/configclient/MessageController.java
@@ -0,0 +1,24 @@
+package com.noh.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RefreshScope
+@RequestMapping("/rest")
+@RestController
+public class MessageController {
+
+    @Value("${message}")
+    private String msg;
+
+    @GetMapping("/msg")
+    public String message() {
+        System.out.println("hello");
+        return msg;
+    }
+
+}
